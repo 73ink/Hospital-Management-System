80ed15fb1d980a01f338a6b71956eafbf3fab6d0
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 16:59:53 +0400

    Method of editAppointment was added

--- src/Services/AppointmentService.java
+++ src/Services/AppointmentService.java
@@ -10,4 +10,15 @@ public class AppointmentService {
         appointments.add(appointment);
         System.out.println("Appointment added successfully.");
     }
+
+    public void editAppointment(String appointmentId, Appointment updatedAppointment) {
+        for (int i = 0; i < appointments.size(); i++) {
+            if (appointments.get(i).getAppointmentId().equals(appointmentId)) {
+                appointments.set(i, updatedAppointment);
+                System.out.println("Appointment updated successfully.");
+                return;
+            }
+        }
+        System.out.println("Appointment not found.");
+    }
 }
