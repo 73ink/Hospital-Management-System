5aae206a90fd546e3a026210a6eca12993c6d5c9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 10:25:21 +0400

    Patient class extends Person, Added specific fields

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -1,7 +1,17 @@
 package Entity;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class Patient {
+public class Patient extends Person {
+    private String patientId;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private String insuranceId;
+    private List<MedicalRecord> medicalRecords;
+    private List<Appointment> appointments;
 
 }
