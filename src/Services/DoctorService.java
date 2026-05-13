da75edde2a421b88a8b8e4954babf2c3be3a31e2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 13:08:14 +0400

    Task 1.8: DoctorService class with CRUD operations was created

---
+++ src/Services/DoctorService.java
@@ -0,0 +1,9 @@
+package Services;
+
+import Entity.Doctor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoctorService {
+    private List<Doctor> doctors = new ArrayList<>();
+}
