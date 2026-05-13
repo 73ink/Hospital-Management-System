189b6c4b24a917498bad7c388d40f153387acc99
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 12:13:09 +0400

    Implement Getter and Setter methods for all fields

--- src/Entity/Department.java
+++ src/Entity/Department.java
@@ -30,5 +30,59 @@ public class Department {
         System.out.println("-----------------");
     }
 
+    public String getDepartmentId() {
+        return departmentId;
+    }
 
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHeadDoctorId() {
+        return headDoctorId;
+    }
+
+    public void setHeadDoctorId(String headDoctorId) {
+        this.headDoctorId = headDoctorId;
+    }
+
+    public List<String> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<String> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<String> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(List<String> nurses) {
+        this.nurses = nurses;
+    }
+
+    public int getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(int bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+
+    public int getAvailableBeds() {
+        return availableBeds;
+    }
+
+    public void setAvailableBeds(int availableBeds) {
+        this.availableBeds = availableBeds;
+    }
 }
