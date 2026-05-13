d93a6f8357fc37e7ef9931082db2018542b6e59d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 15:35:19 +0400

    Added method of addDoctor

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -7,6 +7,10 @@ import java.util.List;
 public class DoctorService {
     private List<Doctor> doctors = new ArrayList<>();
 
+    public void addDoctor(Doctor doctor){
+        doctors.add(doctor);
+        System.out.println("Doctor added successfully.");
+    }
 
 
     public List<Doctor> getDoctors() {
