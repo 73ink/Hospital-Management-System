ef89f563c0f386564aae7200167541a83aebd939
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 11:43:33 +0400

    Task 1.4: Nurse class extends Person

---
+++ src/Entity/Nurse.java
@@ -0,0 +1,14 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nurse extends Person{
+    private String nurseId;
+    private String departmentId;
+    private String shift;
+    private String qualification;
+    private List assignedPatients;
+
+}
