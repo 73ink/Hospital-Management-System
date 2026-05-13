380f0ca32c325ad3d160b468964129e87df331e9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 12:12:03 +0400

    Implement displayInfo() method

--- src/Entity/Department.java
+++ src/Entity/Department.java
@@ -19,5 +19,16 @@ public class Department {
         this.bedCapacity = bedCapacity;
         this.availableBeds = availableBeds;
     }
+    public void displayInfo() {
+        System.out.println("Department ID: " + departmentId);
+        System.out.println("Department Name: " + departmentName);
+        System.out.println("Head Doctor ID: " + headDoctorId);
+        System.out.println("Doctors: " + doctors);
+        System.out.println("Nurses: " + nurses);
+        System.out.println("Bed Capacity: " + bedCapacity);
+        System.out.println("Available Beds: " + availableBeds);
+        System.out.println("-----------------");
+    }
+
 
 }
