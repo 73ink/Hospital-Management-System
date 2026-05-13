7e467796f5e4e813a2e3a831898d54bce17cc34f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 10:06:07 +0400

    Implement equals method and hashCode method

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -112,6 +112,16 @@ public void displayInfo(){
                 ", address='" + address + '\'' +
                 '}';
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
 
 
