fdf5f60624feded328614680af6b44829385cc72
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-13 16:34:34 +0400

    Method of editNurse was added

--- src/Services/NurseService.java
+++ src/Services/NurseService.java
@@ -14,5 +14,16 @@ public class NurseService {
         System.out.println("Nurse added successfully.");
     }
 
+    public void editNurse(String nurseId, Nurse updatedNurse) {
+        for (int i = 0; i < nurses.size(); i++) {
+            if (nurses.get(i).getNurseId().equals(nurseId)) {
+                nurses.set(i, updatedNurse);
+                System.out.println("Nurse updated successfully.");
+                return;
+            }
+        }
+        System.out.println("Nurse not found.");
+    }
+
 
 }
