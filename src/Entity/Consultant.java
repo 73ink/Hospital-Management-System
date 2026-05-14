fe0ea24901526cb248f02169c1b207872ac8c189
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-14 11:14:33 +0400

    Constructor chaining and super

--- src/Entity/Consultant.java
+++ src/Entity/Consultant.java
@@ -2,5 +2,16 @@ package Entity;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Consultant extends Doctor{
+    private List<String> consultationTypes;
+    private boolean onlineConsultationAvailable;
+    private int consultationDuration;
+
+    public Consultant(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List availableSlots, List assignedPatients, List<String> consultationTypes, boolean onlineConsultationAvailable, int consultationDuration) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
 }
