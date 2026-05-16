fb0f7a6055ccbf4a421ef5c699a4c6a1ee6fc4da
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-16 10:24:06 +0400

    Added the required Additional fields

--- src/Entity/GeneralPractitioner.java
+++ src/Entity/GeneralPractitioner.java
@@ -2,4 +2,7 @@ package Entity;
 import java.time.LocalDate;
 
 public class GeneralPractitioner extends Doctor{
+    private boolean walkinAvailable;
+    private boolean homeVisitAvailable;
+    private boolean vaccinationCertified;
 }
