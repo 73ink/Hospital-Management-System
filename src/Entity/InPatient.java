6344dc70d2c95adea1042162d6a3de1ebe13279b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-17 07:54:15 +0400

    Added the main/ additional fields

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -2,4 +2,10 @@ package Entity;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 public class InPatient extends Patient{
+    private LocalDate admissionDate;
+    private LocalDate dischargeDate;
+    private String roomNumber;
+    private String bedNumber;
+    private String admittingDoctorId;
+    private double dailyCharges;
 }
