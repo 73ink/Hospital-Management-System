fff875b8f5d59c6ae864bfb5168c74d5aa15c382
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-17 07:59:17 +0400

    added the getter and setter

--- src/Entity/InPatient.java
+++ src/Entity/InPatient.java
@@ -48,5 +48,54 @@ public class InPatient extends Patient{
         return calculateStayDuration() * dailyCharges;
     }
 
+    // the getter and setter
 
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public LocalDate getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(LocalDate dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    public String getAdmittingDoctorId() {
+        return admittingDoctorId;
+    }
+
+    public void setAdmittingDoctorId(String admittingDoctorId) {
+        this.admittingDoctorId = admittingDoctorId;
+    }
+
+    public double getDailyCharges() {
+        return dailyCharges;
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+        this.dailyCharges = dailyCharges;
+    }
 }
