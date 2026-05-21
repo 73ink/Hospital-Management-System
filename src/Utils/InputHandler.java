b432bed588cdff17a20a7a9f96237758326ccc73
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-21 10:00:36 +0400

    Create an InputHandler class using Scanner

--- src/Utils/InputHandler.java
+++ src/Utils/InputHandler.java
@@ -1,4 +1,14 @@
 package Utils;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class InputHandler {
+
+    private Scanner input;
+
+    public InputHandler() {
+        input = new Scanner(System.in);
+    }
+
 }
