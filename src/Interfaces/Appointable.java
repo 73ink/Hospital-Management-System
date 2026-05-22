c4768e2eefed415b3dc5ec0dee0bfdff37ea60eb
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-22 21:10:27 +0400

    updated from class to interface

--- src/Interfaces/Appointable.java
+++ src/Interfaces/Appointable.java
@@ -3,7 +3,7 @@ package Interfaces;
 import Entity.Appointment;
 import java.time.LocalDate;
 
-public class Appointable {
+public interface Appointable {
     void scheduleAppointment(Appointment appointment);
     void cancelAppointment(String appointmentId);
     void rescheduleAppointment(String appointmentId, LocalDate newDate);
